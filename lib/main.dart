aed39f8b18e8bca277aa897b4bb6a48735b8f46e
AshrafulKabirAshik <ashrafulkabir390@gmail.com>
2024-03-06 01:52:28 +0600

    Second 2.1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Phone Book',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
